7bdb79697bda5d8c7c38fab22a35cee7e782d662
klen-list <klenlist@pika-soft.ru>
2022-06-12 22:37:31 +0300

    convert spaces to tabs

--- lua/autorun/client/context_multiselect.lua
+++ lua/autorun/client/context_multiselect.lua
@@ -5,7 +5,7 @@ local curselect = {}
 local cvar_selection_color = CreateConVar("multiselection_color", "43 156 248 255", FCVAR_ARCHIVE, "Sets the color of context menu multi select halo (RGBA)")
 
 local function GetColorFromStr(str)
-    local r, g, b, a = string.match(str, "(%d+)% (%d+)% (%d+)% *(%d*)")
+	local r, g, b, a = string.match(str, "(%d+)% (%d+)% (%d+)% *(%d*)")
 	if r and g and b then
 		return Color(r, g, b, a)
 	end
@@ -19,13 +19,13 @@ cvars.AddChangeCallback("multiselection_color", function(_, __, new)
 end, "update_color")
 
 hook.Add("PlayerBindPress", "ContextMultiSelect_BlockCtrl", function(_, __, pressed, code)
-    if g_ContextMenu:IsVisible() and code == KEY_LCONTROL and pressed then return true end
+	if g_ContextMenu:IsVisible() and code == KEY_LCONTROL and pressed then return true end
 end)
 
 hook.Remove("CreateMove", "ContextMultiSelect_DisableDuck")
 
 hook.Add("PreDrawHalos", "ContextMultiSelect_DrawCurrent", function()
-    halo.Add(curselect, selection_color, 4 + math.sin(RealTime() * 20), 4 + math.sin(RealTime() * 20), 2)
+	halo.Add(curselect, selection_color, 4 + math.sin(RealTime() * 20), 4 + math.sin(RealTime() * 20), 2)
 end)
 
 hook.Add("OnContextMenuClose", "ContextMultiSelect_ClearCurrent", function() table.Empty(curselect) end)
@@ -36,23 +36,23 @@ local function AddToggleOption(data, menu, ent, ply, tr)
 		menu.ToggleSpacer:SetZPos(500)
 	end
 	local option = menu:AddOption(data.MenuLabel, function()
-        if table.IsEmpty(curselect) then
-            data:Action(ent, tr)
-        else
-            for i, sel_ent in ipairs(curselect) do
-                data:Action(sel_ent, tr)
-            end
-        end
-    end)
-    local checked = false
-    if table.IsEmpty(curselect) then
-        checked = data:Checked(ent, ply)
-    else
-        for i, sel_ent in ipairs(curselect) do
-            checked = data:Checked(sel_ent, ply)
-            if not checked then break end
-        end
-    end
+		if table.IsEmpty(curselect) then
+			data:Action(ent, tr)
+		else
+			for i, sel_ent in ipairs(curselect) do
+				data:Action(sel_ent, tr)
+			end
+		end
+	end)
+	local checked = false
+	if table.IsEmpty(curselect) then
+		checked = data:Checked(ent, ply)
+	else
+		for i, sel_ent in ipairs(curselect) do
+			checked = data:Checked(sel_ent, ply)
+			if not checked then break end
+		end
+	end
 	option:SetChecked(checked)
 	option:SetZPos(501)
 	return option
@@ -64,14 +64,14 @@ local function AddOption( data, menu, ent, ply, tr )
 		menu:AddSpacer()
 	end
 	local option = menu:AddOption(data.MenuLabel, function()
-        if table.IsEmpty(curselect) then
-            data:Action(ent, tr)
-        else
-            for i, sel_ent in ipairs(curselect) do
-                data:Action(sel_ent, tr)
-            end
-        end
-    end)
+		if table.IsEmpty(curselect) then
+			data:Action(ent, tr)
+		else
+			for i, sel_ent in ipairs(curselect) do
+				data:Action(sel_ent, tr)
+			end
+		end
+	end)
 	if data.MenuIcon then
 		option:SetImage(data.MenuIcon)
 	end
@@ -85,15 +85,15 @@ function properties.OpenEntityMenu(ent, tr)
 	local menu = DermaMenu()
 	for k, v in SortedPairsByMemberValue(properties.List, "Order") do
 		if not v.Filter then continue end
-        if table.IsEmpty(curselect) then
-            if not v:Filter(ent, LocalPlayer()) then continue end
-        else
-            local skip = false
-            for i, sel_ent in ipairs(curselect) do
-                if not v:Filter(sel_ent, LocalPlayer()) then skip = true break end
-            end
-            if skip then continue end
-        end
+		if table.IsEmpty(curselect) then
+			if not v:Filter(ent, LocalPlayer()) then continue end
+		else
+			local skip = false
+			for i, sel_ent in ipairs(curselect) do
+				if not v:Filter(sel_ent, LocalPlayer()) then skip = true break end
+			end
+			if skip then continue end
+		end
 		local option = AddOption(v, menu, ent, LocalPlayer(), tr)
 		if v.OnCreate then v:OnCreate(menu, option) end
 	end
@@ -102,27 +102,27 @@ end
 
 g_original_context_hook = g_original_context_hook or hook.GetTable()["GUIMousePressed"]["PropertiesClick"]
 hook.Add("GUIMousePressed", "PropertiesClick", function(code, vec)
-    if not g_ContextMenu:IsVisible() then return end
-    if not input.IsControlDown() then
-        for i, e in ipairs(curselect) do
-            if not IsValid(e) then
-                table.remove(curselect, i)
-            end
-        end
-        return g_original_context_hook(code, vec)
-    end
-    if code == MOUSE_RIGHT then
-        local ent = properties.GetHovered(EyePos(), vec)
-        if IsValid(ent) then
-            for i, e in ipairs(curselect) do
-                if not IsValid(e) then
-                    table.remove(curselect, i)
-                elseif e == ent then
-                    table.remove(curselect, i)
-                    return
-                end
-            end
-            table.insert(curselect, ent)
-        end
-    end
+	if not g_ContextMenu:IsVisible() then return end
+	if not input.IsControlDown() then
+		for i, e in ipairs(curselect) do
+			if not IsValid(e) then
+				table.remove(curselect, i)
+			end
+		end
+		return g_original_context_hook(code, vec)
+	end
+	if code == MOUSE_RIGHT then
+		local ent = properties.GetHovered(EyePos(), vec)
+		if IsValid(ent) then
+			for i, e in ipairs(curselect) do
+				if not IsValid(e) then
+					table.remove(curselect, i)
+				elseif e == ent then
+					table.remove(curselect, i)
+					return
+				end
+			end
+			table.insert(curselect, ent)
+		end
+	end
 end)
